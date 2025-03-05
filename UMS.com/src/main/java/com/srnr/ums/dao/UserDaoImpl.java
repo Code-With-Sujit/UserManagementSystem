c5a75e3fd77b20217bd3e0ee6915bb02df3e3364
karunya <karunyalingutla02@gmail.com>
2025-03-05 15:27:54 +0530

    added some crud operations

--- UMS.com/src/main/java/com/srnr/ums/dao/UserDaoImpl.java
+++ UMS.com/src/main/java/com/srnr/ums/dao/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.srnr.ums.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,75 @@ public class UserDaoImpl implements UserDao{
 			throw new RuntimeException("User Can not be null");
 		}
 		
+	}
+
+	@Override
+	public Optional<String> deleteUser(String userId) {
+		
+		if(userId!=null && !userId.isBlank()) 
+		{
+			Optional<User> byId = userRepository.findById(userId);
+			if(byId.isPresent()) 
+			{
+				userRepository.delete(byId.get());
+				return Optional.of("user deleted Successfully with userID"+userId);
+			}
+			else {
+				throw new RuntimeException("User not exist with id : "+userId);
+			}
+		}
+		else {
+			throw new RuntimeException("UserId can not be null or blank");
+		}	
+	}
+
+	@Override
+	public Optional<User> findUserById(String userId) {
+		if(userId!=null && !userId.isBlank()) {
+			Optional<User> byId = userRepository.findById(userId);
+			if(byId.isPresent()) {
+				User referenceById = userRepository.getReferenceById(userId);
+				return Optional.of(referenceById);
+			}
+			else {
+				throw new RuntimeException("user with userId"+userId+"not existed in database");
+			}
+		}
+		else {
+			throw new RuntimeException("userId can't be null.please provide userId.");
+		}
+	}
+
+	@Override
+	public Optional<List<User>> findAllUser() {
+		List<User> all = userRepository.findAll();
+		if(all!=null && all.size() > 0) 
+		{
+			return Optional.of(all);
+		}
+		else {
+			throw new RuntimeException("No User exist ! ");
+		}
+	}
+
+	@Override
+	public Optional<User> updateUser(User user) {
+	    if (user!=null) {
+	    	Optional<User> existingUser = userRepository.findById(user.getUserId());
+
+		    if (existingUser.isPresent()) {
+		        User updatedUser = existingUser.get();
+		        updatedUser.setUserName(user.getUserName());
+		        User savedUser = userRepository.save(updatedUser);
+		        
+		        return   savedUser!=null ?  Optional.of(savedUser) : Optional.empty();
+		    } else {
+		        throw new RuntimeException("User with ID " + user.getUserId() + " not found");
+		    }
+	    }
+	    else {
+	    	throw new RuntimeException("user must not be null");
+	    }   
 	}
 
 }

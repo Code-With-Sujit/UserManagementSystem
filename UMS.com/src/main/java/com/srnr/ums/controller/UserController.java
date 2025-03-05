7477f648bd6f9ef27305fb2d98155dbbddaa53c9
Naga Vethendra <vethendrapattamsetti@gmail.com>
2025-03-05 14:12:41 +0530

    Usercontroller is created only for register user

--- UMS.com/src/main/java/com/srnr/ums/controller/UserController.java
+++ UMS.com/src/main/java/com/srnr/ums/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.srnr.ums.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,15 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.srnr.ums.dto.UserRequestDTO;
+import com.srnr.ums.dto.UserResponseDTO;
+import com.srnr.ums.userservice.UserServiceI;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 	
+	
+	private UserServiceI userService;
 	@PostMapping(value = "/registerUser")
-	public ResponseEntity<?> registerUser(@RequestBody UserRequestDTO dto)
+	public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO dto)
 	{
-		return null;
+		UserResponseDTO user = this.userService.saveUser(dto);
+		return new ResponseEntity<UserResponseDTO>(user,HttpStatus.OK);
+		
 	}
 
 }

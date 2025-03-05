55423aee630a0ba3f0eb69e7b7629d2fd39d44bb
venu kumar <k.venukumar6543@gmail.com>
2025-03-05 13:58:41 +0530

    first commit added service layer

---
+++ UMS.com/src/main/java/com/srnr/ums/userservice/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.srnr.ums.userservice;
+
+import com.srnr.ums.dto.UserRequestDTO;
+import com.srnr.ums.dto.UserResponseDTO;
+
+
+public class UserServiceImpl implements UserServiceI {
+
+	@Override
+	public UserResponseDTO saveUser(UserRequestDTO userRequestDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+
+}
